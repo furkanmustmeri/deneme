2713ddc203b216bfe8ddc8a48e9b15019c1f844d
furkan <furkankaradeniz@tuta.io>
2023-11-02 15:52:49 +0200

    Day 3

---
+++ pizza order program.py
@@ -0,0 +1,36 @@
+""" Based on a user's order, work out their final bill.
+Small pizza (S): $15
+Medium pizza (M): $20
+Large pizza (L): $25
+Add pepperoni for small pizza (Y or N): +$2
+Add pepperoni for medium or large pizza (Y or N): +$3
+Add extra cheese for any size pizza (Y or N): +$1 """
+
+print("Welcome To Furkan's Pizza!")
+
+bill=0
+
+size = input("What kind of pizza do you want? S, M or L \n")
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+add_pepperoni = input("Would you like to have pepperoni? Y or N\n")
+if add_pepperoni == "Y":
+    if size == "S":
+        bill +=2
+    else:
+        bill +=3
+else:
+    bill == bill
+
+extra_cheese = input ("Want some extra cheese? Y or N\n")
+if extra_cheese == "Y":
+    bill +=1
+else:
+    bill == bill
+
+print(f"Your final bill is: ${bill}.")
